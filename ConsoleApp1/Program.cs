087acc6c97d95d87a7284021194e09ae96627534
Eric Winnington <eric.in.the.net@gmail.com>
2020-03-30 09:20:09 +0200

    First commit - client & server side blazor Server side - Roslyn and IronPython work Client side (wasm) - IronPython works - Roslyn gives error

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using IronPython.Hosting;
+using System;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string SourceCodePy = "x = 5+4"; 
+            
+            string v1 = @"import sys
+
+def Simple():
+	print ""Hello from Python""
+	print ""Call Dir(): ""
+	print dir()
+	print ""Print the Path: "" 
+	print sys.path
+
+Simple()";
+            var engine = Python.CreateEngine();
+            var scope = engine.CreateScope();
+            var source = engine.CreateScriptSourceFromString(SourceCodePy, Microsoft.Scripting.SourceCodeKind.Statements);
+            var compiled = source.Compile();
+
+            var result = compiled.Execute(scope);
+            bool isAvailable = scope.TryGetVariable("x", out dynamic output);
+
+            if(isAvailable)
+                Console.WriteLine(output);
+        }
+    }
+}
